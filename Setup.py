5e3d8feb6ea07a44b8007eaa7f21abe40cafaa4a
Spider <31970470+SpiderPL@users.noreply.github.com>
2018-12-03 20:51:42 +0100

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="Pygit-zadanie",
+    version="1.0",
+    author="Adam Pajak",
+    author_email="pajak.adam.emil@gmail.com",
+    scripts=['Pygit.py']
+
+)
